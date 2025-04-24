8ee5656db0de2c422cf66cd4a6c2c42419a0ca71
Alejandro García Serrano <agscm2@gmail.com>
2025-04-24 18:46:10 +0200

    Upload pre descanso

--- Ejercicio1.mongodb.js
+++ Ejercicio1.mongodb.js
@@ -1,3 +1,5 @@
+use ('test')
+
 /* db.usuarios.insertMany([
     {
       nombre: "Ana",
@@ -22,6 +24,27 @@
     }
   ]); */
 
+// Ejercicios Clase del 23-04-2025
+
+
   // Parte de lectura de documentos
   // Método find sin parametros: Obtenemos toda la información de la colección
-  db.usuarios.find();
+  // ---> db.usuarios.find();
+
+  // Segundo parametro del fin se usa para mostrar la clientInformation,macion, 1 muestra y 0 oculta
+  // ---> db.usuarios.find({}, {nombre: 1, apellido: 1, _id: 0});
+
+  // Primer parametro se usa para filtar la información
+  // ---> db.usuarios.find({ nombre: "Luis"});
+  // ---> db.usuarios.find({ nombre: "Luis"}, {nombre: 1, apellido: 1, _id: 0});
+
+/*   Condicionales:
+  $gt: Mayor que
+  $lt: Menor que
+  $eq: Igual a 
+  $ne: Diferente a 
+  $gte: Mayor o igual que 
+  $lte: Menor o igual que  */
+
+  db.usuarios.find({ activo: false, edad: { $gt: 30}});
+  db.usuarios.find({ activo: true, edad: { $lt: 30}});
